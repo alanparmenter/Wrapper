ed32343966a7a50512cac34e47f0b09e9e789f4d
= <=>
2024-11-07 11:38:33 +0000

    Update gpt.py

--- gpt.py
+++ gpt.py
@@ -7,22 +7,21 @@ load_dotenv('.env', override=True)
 openai_api_key = os.getenv("OPENAI_API_KEY")
 client = openai.OpenAI(api_key = openai_api_key)
 
-def clear_screen():
-    if os.name == 'nt':  # Windows
-        os.system('cls')
-    else:  # Linux and macOS
-        os.system('clear')
-clear_screen()
+if os.name == 'nt':  # Windows
+    os.system('cls')
+else:  # Linux and macOS
+    os.system('clear')
 
 def get_llm_response(prompt):
     completion = client.chat.completions.create(
         model="gpt-4o",
         messages=[{ "role": "user", "content": prompt }],
-        temperature=0 # 1.0 gives more random output
+        temperature=0, # 1.0 gives more random output
+        #max_tokens=100 # uncomment to truncate response
     )
     response = completion.choices[0].message.content
     return response
-    
+
 while True:
     prompt = input('Prompt: ')
     with open('gpt_output.txt', 'a') as file:
